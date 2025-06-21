0a23861293f46adbc1ade8db3c22fee19636d59f
Icbitic <icbitic@foxmail.com>
2025-06-21 18:57:59 +0800

    Initial commit

---
+++ src/main/java/org/bedrock/teateach/TeaTeachApplication.java
@@ -0,0 +1,13 @@
+package org.bedrock.teateach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaTeachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bedrock/teateach/TeaTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedrock.teateach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaTeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
