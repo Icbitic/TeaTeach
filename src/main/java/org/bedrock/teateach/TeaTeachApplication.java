91198b16fe76ec1bbff4d0bfb1f04835123df67b
Icbitic <icbitic@foxmail.com>
2025-06-22 21:12:06 +0800

    add support for Spring Doc

--- src/main/java/org/bedrock/teateach/TeaTeachApplication.java
+++ src/main/java/org/bedrock/teateach/TeaTeachApplication.java
@@ -3,9 +3,13 @@ package org.bedrock.teateach;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("org.bedrock.teateach.mappers")
+@EnableCaching
+@EnableAspectJAutoProxy
 public class TeaTeachApplication {
 
     public static void main(String[] args) {
